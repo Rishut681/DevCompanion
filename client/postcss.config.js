bcc59e036a518b9ec08974aab2068c2ae5882367
Rishu Raj <131179273+Rishut681@users.noreply.github.com>
2025-12-04 17:32:55 +0530

    Update postcss.config.js

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,10 +1,6 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./client/src/**/*.{js,jsx,ts,tsx}", // include everything under src
-  ],
-  theme: {
-    extend: {},
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
-  plugins: [],
 };
